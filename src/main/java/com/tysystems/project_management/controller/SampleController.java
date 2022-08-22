32fa9c7b9d818d6db9585d8183d184cd0f24f68a
lachicadulce <sybaek94@naver.com>
2022-08-22 16:40:46 +0900

    modify compare.html and SampleController.java to update database when some users resign or join this company.

--- src/main/java/com/tysystems/project_management/controller/SampleController.java
+++ src/main/java/com/tysystems/project_management/controller/SampleController.java
@@ -1,7 +1,9 @@
 package com.tysystems.project_management.controller;
 
+import com.tysystems.project_management.domain.OrgInfo;
 import com.tysystems.project_management.domain.PL_USER;
 import com.tysystems.project_management.domain.UserKey;
+import com.tysystems.project_management.service.OrgInfoService;
 import com.tysystems.project_management.service.PL_USERService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,53 +23,92 @@ public class SampleController {
 
     @Autowired
     PL_USERService service;
+    @Autowired
+    OrgInfoService orgInfoService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/compare")
     public void compare(Model model){
         model.addAttribute("users", service.getLists());
     }
 
-    @RequestMapping( value = "/user/delete", method = RequestMethod.GET)
-    public String deleteUser(@RequestParam String user_code, @RequestParam String user_date){
-        UserKey userKey = new UserKey();
-        userKey.setUser_code(user_code);
-        userKey.setUser_date(user_date);
-        userKey.setCompany("TSS");
-        userKey.setBusiness_unit("TSSBU");
+    /**
+     * 재직자는 추가해주고 퇴사자는 end_date와 live_gb 컬럼을 업데이트 해주는 컨트롤러
+     * @param user_codes 사번 리스트
+     * @param user_names 사원명 리스트
+     * @param org_nms 부서명 리스트
+     * @param start_dates 입사일 리스트
+     * @param user_dates 입력 날짜 리스트
+     * @param end_dates 퇴사 날짜 리스트
+     * @param user_descs 직책 리스트
+     * @return 다시 파일을 선택할 수 있는 화면으로 redirect
+     * @throws ParseException
+     */
+    @RequestMapping(value = "/user/update", method = RequestMethod.POST)
+    public String updateUser(@RequestParam(value = "user_code") String[] user_codes,
+                             @RequestParam(value = "user_name") String[] user_names,
+                             @RequestParam(value = "org_nm") String[] org_nms,
+                             @RequestParam(value = "start_date") String[] start_dates,
+                             @RequestParam(value = "user_date") String[] user_dates,
+                             @RequestParam(value = "end_date") String[] end_dates,
+                             @RequestParam(value = "user_desc") String[] user_descs) throws ParseException {
 
-        service.delete(userKey);
+        for (int i = 1; i < user_codes.length; i++){
+            if (user_names[i].isEmpty()){ // 퇴사자일경우
 
-        return "redirect:/compare";
-    }
+                System.out.println("=====[end user info]=====");
+                System.out.println("user_code: " + user_codes[i]);
+                System.out.println("user_date: " + user_dates[i]);
+                System.out.println("end_date: " + end_dates[i]);
+                System.out.println("=====================");
 
-    @RequestMapping(value = "/user/save", method = RequestMethod.GET)
-    public String saveUser(Model model, @RequestParam String user_code, @RequestParam String user_date,
-                           @RequestParam String user_name, @RequestParam String org_nm, @RequestParam String user_desc,
-                           @RequestParam Date start_date, @RequestParam String join_date, @RequestParam String org_code,
-                           @RequestParam String deptid_s, @RequestParam String deptid_s_nm, @RequestParam String deptid_b,
-                           @RequestParam String deptid_b_nm
-    ) throws ParseException {
-        PL_USER pl_user = new PL_USER("TSS", "TSSBU", user_date, user_code, user_name, org_code, org_nm, user_desc, deptid_s, deptid_s_nm, deptid_b, deptid_b_nm, start_date, new SimpleDateFormat("yyyyMMdd").parse("20991231"), new Date(), null, null, null, "Y", join_date);
-        service.save(pl_user);
+                UserKey userKey = new UserKey();
+                userKey.setUser_date(user_dates[i]);
+                userKey.setUser_code(user_codes[i]);
+                userKey.setCompany("TSS");
+                userKey.setBusiness_unit("TSSBU");
 
-        return "redirect:/compare";
-    }
+                PL_USER pl_user = service.getList(userKey);
+                System.out.println(pl_user.getUser_name());
+                if (pl_user.getUser_name() == null){
+                    System.out.println("no exist user");
+                    continue;
+                }
+                pl_user.setLive_gb("N");
+                pl_user.setEnd_date(new SimpleDateFormat("yyyy-MM-dd").parse(end_dates[i]));
 
-    // 퇴사자 업데이트
-    @RequestMapping(value = "/user/update", method = RequestMethod.GET)
-    public String updateUser(@RequestParam String user_code, @RequestParam String user_date, @RequestParam String end_date) throws ParseException {
-        UserKey userKey = new UserKey();
-        userKey.setUser_date(user_date);
-        userKey.setUser_code(user_code);
-        userKey.setCompany("TSS");
-        userKey.setBusiness_unit("TSSBU");
+                service.save(pl_user);
+            } else { // 입사자일경우
+                System.out.println("*****[start user info]*****");
+                System.out.println("user_code: " + user_codes[i]);
+                System.out.println("org_name: " + org_nms[i]);
 
-        PL_USER pl_user = service.getList(userKey);
-        pl_user.setLive_gb("N");
-        pl_user.setEnd_date(new SimpleDateFormat("yyyyMMdd").parse(end_date));
+                PL_USER pl_user = new PL_USER();
+                OrgInfo orgInfo = orgInfoService.getList(org_nms[i]);
 
-        service.save(pl_user);
+                System.out.println("org_code: " + orgInfo.getOrg_code());
+                System.out.println("**********************");
 
+
+                pl_user.setUser_date(start_dates[i].replace("-", ""));
+                pl_user.setUser_code(user_codes[i]);
+                pl_user.setUser_name(user_names[i]);
+                pl_user.setOrg_nm(org_nms[i]);
+                if (!user_descs[i].equals(" "))
+                    pl_user.setUser_desc(user_descs[i]);
+                pl_user.setStart_date(new SimpleDateFormat("yyyy-MM-dd").parse(start_dates[i]));
+                pl_user.setEnd_date(new SimpleDateFormat("yyyy-MM-dd").parse(end_dates[i]));
+                pl_user.setJoin_date(start_dates[i].replace("-", ""));
+                pl_user.setCompany("TSS");
+                pl_user.setBusiness_unit("TSSBU");
+                pl_user.setLive_gb("Y");
+                pl_user.setOrg_code(orgInfo.getOrg_code());
+                pl_user.setDeptid_s(orgInfo.getParent_org_code());
+                pl_user.setDeptid_s_nm(orgInfo.getParent_org_name());
+                pl_user.setDeptid_b(orgInfo.getRoot_org_code());
+                pl_user.setDeptid_b_nm(orgInfo.getRoot_org_name());
+                service.save(pl_user);
+            }
+        }
         return "redirect:/compare";
     }
 }
